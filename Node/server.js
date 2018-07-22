2227fdfe5c30d9f0cfdbc60fb29d79daa942e030
Avyaan <avi@Avyaans-MacBook-Pro.local>
2018-07-22 11:02:04 +0530

    a

--- Node/server.js
+++ Node/server.js
@@ -2,6 +2,9 @@ var http = require('http');
 var express = require('express');
 var chalk = require('chalk');
 var ejs = require('ejs');
+const bodyParser = require('body-parser');
+const debug = require('debug')('app')
+
 var app = express();
 var port= 5600;
 
@@ -11,9 +14,14 @@ var nav = [
     {link:'/admin',title:'Admin'}
 ];
 
+require('./src/config/passport.js')(app)
+
 var productsRouter = require('./src/routes/ProductsRoutes')(nav);
 var adminRouter = require('./src/routes/adminRoutes')(nav);
+var authRouter = require('./src/routes/authRoutes')(nav);
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:true }));
 app.use(express.static(__dirname + '/public'));
 app.set('views', './src/views')
 app.set('view engine', 'ejs')
@@ -22,6 +30,7 @@ app.set('view engine', 'ejs')
 
 app.use('/products',productsRouter);
 app.use('/admin', adminRouter);
+app.use('/auth', authRouter)
 
 app.get('/', function(req,res){
     res.render('index',
